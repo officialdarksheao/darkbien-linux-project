8b2650aeb4d400aedef3d5773a8bc75a1d7fa501
Philip Wrinkle <philip.wrinkle@gmail.com>
2019-01-24 19:18:05 -0500

    bootscripts source change

--- build-bootscripts.sh
+++ build-bootscripts.sh
@@ -3,8 +3,8 @@ echo "Setting up bootscripts"
 
 CURRENT_DIR=$(pwd)
 cd ${SOURCE_DIR}
-tar -xvf ${SOURCE_DIR}/clfs-embedded-bootscripts-1.0-pre3.tar.bz2
-cd ${SOURCE_DIR}/clfs-embedded-bootscripts-1.0-pre3
+tar -xvf ${SOURCE_DIR}/clfs-embedded-bootscripts-1.0-pre5.tar.bz2
+cd ${SOURCE_DIR}/clfs-embedded-bootscripts-1.0-pre5
 
 make DESTDIR=${BUILD_DIR}/ install-bootscripts
 ln -sv ../rc.d/startup ${BUILD_DIR}/etc/init.d/rcS
